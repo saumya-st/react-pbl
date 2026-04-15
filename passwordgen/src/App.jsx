c7a05fad9ac491c9631bd71675b567438f65ae6c
saumya-st <saumyacodes02@gmail.com>
2026-04-15 23:20:37 +0530

    heyy

---
+++ passwordgen/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [length, setlength] = useState(8)
+const[number , setnumber] = useState(false)
+const[char , setchar] = useState
+  return (
+    <>
+      <h1 className='text-4xl text-center'>Password generator</h1>
+    </>
+  )
+}
+
+export default App
